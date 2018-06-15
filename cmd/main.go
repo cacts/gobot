992e84e8166511406cf75404af23089f87376c1b
cacts <deansauce@gmail.com>
2018-06-14 21:53:48 -0600

    b

--- cmd/main.go
+++ cmd/main.go
@@ -199,7 +199,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if strings.HasPrefix(m.Content, "<@447429502390370315>") {
 		message := strings.SplitN(m.Content, " ", 2)[1]
 		if strings.HasPrefix(message, "<:") && strings.HasSuffix(message, ">") {
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("@<%v> %v", m.Author.ID, message)))
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("@<%v> %v", m.Author.ID, message))
 		} else {
 			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("@<%v> <:fingerguns:342698356818182156>", m.Author.ID))
 		}
